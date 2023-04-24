ed2e60c52e957093d7c08cd05bae6bc1a9e605da
Hari Krishna K <harikrishnakarur@gmail.com>
2023-04-24 12:08:21 +0530

    add ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,39 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: "/src/index.js", // main js
+  entry: "/src/index.tsx", // main js
   output: {
     path: path.resolve(__dirname, "dist"), // output folder
     publicPath: "/",
   },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"],
+  },
   module: {
     rules: [
+      // {
+      //   test: /\.?js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       presets: ["@babel/preset-env", "@babel/preset-react"],
+      //     },
+      //   },
+      // },
       {
-        test: /\.?js$/,
-        exclude: /node_modules/,
+        test: /\.(js|jsx|ts|tsx)$/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
           },
         },
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
